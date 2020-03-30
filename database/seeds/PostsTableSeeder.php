1d093b5a09433ff42060d6e8dd8ba618448f82dd
Federico <federico.caiello@gmail.com>
2020-03-30 17:48:44 +0200

    route auth

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Seeder;
 use App\Post;
 use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
 class PostsTableSeeder extends Seeder
 {
     /**
@@ -13,6 +15,13 @@ class PostsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-
+      for ($i=0; $i < 10 ; $i++) {
+        $newPost = new Post;
+        $newPost->user_id = 1;
+        $newPost->title = $faker->sentence(3);
+        $newPost->body = $faker->text(255);
+        $newPost->slug = Str::finish(Str::slug($newPost->title), rand(1, 1000000));
+        $newPost->save();
+      }
     }
 }
