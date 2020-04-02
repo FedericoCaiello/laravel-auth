9f8b3dce369bddb073737de9ad4340fca5ef1fc6
Federico <federico.caiello@gmail.com>
2020-04-02 17:07:18 +0200

    seeder tags

--- app/Tag.php
+++ app/Tag.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+  protected $fillable = [
+    'name'
+  ];
+  
   public function posts()
    {
-      protected $fillable = [
-        'name'
-      ];
 
       return $this->belongsToMany('App\Post');
    }
